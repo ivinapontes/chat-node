92f49822931af6ee41c6a4af756de064e07e42a9
Glitch (hello-express) <none>
2019-05-28 19:03:01 +0000

    💸📍 Checkpoint ./server.js:4409256/219

--- server.js
+++ server.js
@@ -41,33 +41,37 @@ app.get("/messages/:id?", function(request, response){
 });
 
 
-// app.delete("/messages/delete/:id?", function(request, response){
-//   let selectedId = request.params.id;
-//   console.log(selectedId)
-//   response.json(messages)
-//   let found = messages.find(message=>message.id == selectedId);
-//     console.log(found);
+app.delete("/messages/delete/:id?", function(request, response){
+  let selectedId = request.params.id;
+  console.log(selectedId)
+  response.json(messages)
+  let found = messages.find(message=>message.id == selectedId);
+    console.log(found);
   
-//   if (found){
-//     messages = messages.filter(message=>message.id != selectedId);
-//     response.status(204).json(messages)  
-//     } else {
-//       response.status(404).json(messages)
-//     }
-// })
-app.delete('/delete/:id?', function (req, res) {
-  let id = req.params.id;
-  
-  var findMessageById = welcomeMessage.find(message => message.id == id);
-  if (findMessageById){
-    let filteredArray = welcomeMessage.filter(message => message.id != findMessageById);
-    res.status(204).json(filteredArray)  
-    console.log(filteredArray)
+  if (found){
+    messages = messages.filter(message=>message.id != selectedId);
+    response.status(204).json(messages)  
     } else {
-      res.status(404).json(messages)
+      response.status(404).json(messages)
     }
+})
+
+// app.delete('/delete/:id?', function (req, res) {
+//   let id = req.params.id;
+//   let filteredArray;
+  
+//   var findMessageById = welcomeMessage.find(message => message.id == id);
+  
+//   if (findMessageById){
+//     console.log(findMessageById)
+//     filteredArray = welcomeMessage.filter(message => message.id != findMessageById);
+//     res.json(filteredArray)  
+//     // console.log(filteredArray)
+//     } else {
+//       res.status(404).json(messages)
+//     }
   
  
-})
+// })
 
 app.listen(process.env.PORT);
